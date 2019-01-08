b91b0b6c895e9df65039c9269b9c9df371bd643d
bgonzalez <31961293+bgonzalez127@users.noreply.github.com>
2019-01-08 09:40:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+//Spell #1
+$("#why").click(function(){
+    $("#netflix").toggle();
+});
+
+//Spell #2
+$("#magicBunny").click(function(){
+    $("#bunny").slideToggle(); 
+});
+
+//Spell #3
+$("#pickacard").click(function(){
+    $("#cards").fadeToggle();    
+});
+
+
+
